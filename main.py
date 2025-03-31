e773038a257e69fd7a832f21842580bc0d3b6f63
Christian Alvarado <ckevar@gmail.com>
2025-03-31 11:59:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,144 @@
-import numpy as np
+import cv2 as cv
+from tqdm import tqdm
+import random
+import time
+import sys
+import os
 
-base_path_train = "train"
-base_path_test = "test"
+from datasetMOT17 import ImgStream
+from datasetMOT17 import dataset_train as dataset
 
-dataset_train = [base_path_train + "/MOT17-02",
-                 base_path_train + "/MOT17-04",
-                 base_path_train + "/MOT17-05",
-                 base_path_train + "/MOT17-09",
-                 base_path_train + "/MOT17-10",
-                 base_path_train + "/MOT17-11",
-                 base_path_train + "/MOT17-13"]
+USER_INPUT_LEN = 3
+CONFIG = {
+    'IMG_640x640': {
+        'dir' : 'letterbox_640x640/',
+        'size' : 640
+        },
+    'IMG_480x480' : {
+        'dir' : 'letterbox_480x480/',
+        'size' : 480
+        }
+} 
+ZERO_PADDING = [0, 0, 0]
 
-dataset_test = [base_path_test + "/MOT17-01",
-                base_path_test + "/MOT17-03",
-                base_path_test + "/MOT17-06",
-                base_path_test + "/MOT17-07",
-                base_path_test + "/MOT17-08",
-                base_path_test + "/MOT17-12",
-                base_path_test + "/MOT17-14"]
+def create_dest_path(src_path, tov, config):
+    aux = src_path.split("/")
+    aux[-1] = aux[-3] + aux[-1]
+    aux[-3] = config['dir'] + tov
+    img_path = "/".join(aux)
 
-class ImgStream:
-    def __init__(self, ds_path):
-        self.path = ds_path
-        self._load_det_(ds_path)
-        self._touch_output_(ds_path)
+    aux[-2] = "labels"
+    aux2 = aux[-1].split(".")
+    aux2[-1] = "txt"
+    aux[-1] = ".".join(aux2)
+    label_path = "/".join(aux)
+
+    return img_path, label_path
+
+def letterbox(ratio, config):
+    yolo_sz = config['size']
+    new_hw = int(yolo_sz / ratio)
+
+    padding_pixels = yolo_sz - new_hw
+    x = padding_pixels // 2
+    y = x + padding_pixels % 2
+
+    return yolo_sz, new_hw, x, y
     
-    def _touch_output_(self, some_path):
-        self.est_file = some_path + "/det/det.txt"
-        f = open(self.est_file, 'w')
-        f.close()
+def process_image(dst, src, config):
+    img = cv.imread(src, cv.IMREAD_COLOR)
+    
+    h, w = img.shape[:2]
+    ratio = w/h
+    top = bottom = left = right = 0
 
-    def _load_det_(self, ds_path):
-        self.dets = np.loadtxt(ds_path + "/gt/gt.txt", delimiter=',')
-        self.frame_count = int(self.dets[:,0].max())
+    if ratio > 1:
+        new_width, new_height, top, bottom = letterbox(ratio, config)
+    else:
+        new_height, new_width, left, right = letterbox(ratio, config)
 
-    def _load_frame_(self):
-        path = self.path + "/img1/" + format(self.frame_i + 1, '06d') + ".jpg" 
-        img = cv.imread(path, cv.IMREAD_COLOR)
-        return img
+    new_img = cv.resize(img, (new_width, new_height))
+    new_img = cv.copyMakeBorder(new_img, 
+                                top, 
+                                bottom,
+                                left, 
+                                right, 
+                                cv.BORDER_CONSTANT, 
+                                value=ZERO_PADDING)
+    cv.imwrite(dst, new_img)
+    return h, w
 
-    def stream(self, subset=1):
-        for self.frame_i in range(int(self.frame_count * subset)):
-            frame_id = self.frame_i + 1
-            yield self.dets[self.dets[:, 0] == frame_id, 2:7], self._load_frame_()
+def process_bbox(path, bbox, h, w):
+    fd = open(path, 'w')
 
-    def streamPath(self, subset=1):
-        for self.frame_i in range(int(self.frame_count * subset)):
-            frame_id = self.frame_i + 1
-            path = self.path + "/img1/" + format(frame_id, '06d') + ".jpg" 
-            yield self.dets[self.dets[:, 0] == frame_id, 2:8], path
+    for box in bbox:
+        xc = box[0] + box[2] / 2.
+        yc = box[1] + box[3] / 2.
+        
+        line = str(int(box[-1])) + ' ' + \
+               str(xc / w)       + ' ' + \
+               str(yc / h)       + ' ' + \
+               str(box[2] / w)   + ' ' + \
+               str(box[3] / h)   + '\n'
+        fd.write(line)
+    fd.close()
 
+def shuffle_train_validation(total, train_len=0.8):
+    if random.random() < train_len:
+        return "train"
 
-    def saveTrackers(self, tracks):
-        with open(self.est_file, 'a') as f:
-            for j in range(len(tracks)):
-                w = tracks[j][2] - tracks[j][0]
-                h = tracks[j][3] - tracks[j][1]
+    return "val"
+    
+def process_dataset(dataset_path, config):
+    stream = ImgStream(dataset_path)
 
-                f.write(str(int(self.frame_i + 1)) + ',' +
-                        str(tracks[j][4]) + ',' +
-                        str(tracks[j][0]) + ',' +
-                        str(tracks[j][1]) + ',' + 
-                        str(w) + ',' +
-                        str(h) + ',' + 
-                        '-1\n')
+    for bbox, src_path in stream.streamPath():
+        tov = shuffle_train_validation(stream.frame_count)
+        img_path, label_path = create_dest_path(src_path, tov, config)
+
+        h, w = process_image(img_path, src_path, config)
+        process_bbox(label_path, bbox, h, w)
+    
+def main(root_dir, config):
+    for ds in tqdm(dataset):
+        process_dataset(root_dir + '/' + ds, config)
+       
+def mkdirs(root_dir, out_dir):
+    target_dir = root_dir + '/train/' + out_dir
+    
+    try:
+        os.makedirs(target_dir + 'train/labels/')
+    except FileExistsError:
+        pass
+    except e:
+        print("\n  Something went wrong when creating train/labels/.", e)
+        exit()
+
+    try:
+        os.makedirs(target_dir + 'val/labels/')
+    except FileExistsError:
+        pass
+    except e:
+        print("\n  Something went wrong when creating val/labels/.", e)
+        exit()
+ 
+def init(root_dir, config):
+    random.seed(time.time())
+    mkdirs(root_dir, config['dir'])
+
+def parse_user_input(argv):
+    configs = list(CONFIG.keys())
+    if (len(argv) < USER_INPUT_LEN) or (argv[2] not in configs):
+        print("\nusage: python3 {} <dir> <imgsz>\n"\
+              "  dir   : directory of MOT-17 dataset\n"\
+              "  imgsz : {}".format(argv[0], configs))
+        exit()
+
+    return argv[1], CONFIG[argv[2]]
+
+if "__main__" == __name__:
+    root_dir, config = parse_user_input(sys.argv)
+
+    init(root_dir, config)
+    main(root_dir, config)
 
